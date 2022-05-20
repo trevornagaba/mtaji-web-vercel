adee06a4cc240cbda3eac71fd4d6e2d10a6fed30
ongebo <isaacongebo@gmail.com>
2022-05-20 21:16:55 +0300

    Update media illustration

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ export default function Home() {
       <LandingNavBar />
 
       <div className="tagline">
+        <div className="tagline-background"></div>
+
         <div className="proposition">
           <h1>
             Invest in Africa&apos;s <br />
@@ -188,21 +190,35 @@ export default function Home() {
           .tagline {
             display: flex;
             height: 100vh;
-            background-image: url("/assets/illustration_2.svg");
+            background-image: url("/assets/illustration_3.svg");
             background-position: right;
-            background-size: 60%;
             background-repeat: no-repeat;
             align-items: center;
+            position: relative;
+          }
+
+          .tagline-background {
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background: #f7f6fe;
+            opacity: 0;
           }
 
           .tagline .proposition {
             width: 50%;
             padding: 32px;
+            background-image: url("/assets/tagline_background.svg");
+            background-position: bottom right;
+            background-repeat: no-repeat;
+            z-index: 1000;
           }
 
           .tagline .proposition h1 {
             color: #09062d;
-            font-size: 54px;
+            font-size: 2.6rem;
             font-weight: 700;
             font-style: bold;
           }
@@ -215,9 +231,10 @@ export default function Home() {
           .companies-section button {
             background: #2518b8;
             border: none;
+            box-shadow: 2px 2px 12px rgba(37, 24, 184, 0.6);
             color: white;
             font-weight: bold;
-            font-size: 19px;
+            font-size: 1.1rem;
             border-radius: 5px;
             cursor: pointer;
             display: block;
@@ -346,6 +363,10 @@ export default function Home() {
               flex-direction: column;
             }
 
+            .tagline-background {
+              opacity: 0.88;
+            }
+
             .tagline .proposition {
               padding: 8px;
               width: 100%;
@@ -376,6 +397,10 @@ export default function Home() {
 
           /* Adjust for tablet screen sizes. */
           @media only screen and (min-width: 600px) and (max-width: 800px) {
+            .tagline-background {
+              opacity: 0.88;
+            }
+
             .tagline .proposition {
               width: 100%;
             }
