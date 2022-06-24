a3950d960296cfddc06d500a42cfdf40f818662e
Nelson M <nelson.mwirumubi@airscape.com.au>
2022-06-24 09:20:48 +0300

    Add tailwind css

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,28 @@
 import "../styles/styles.css";
+import "../styles/global.css";
 import Head from "next/head";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Poppins"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />
-        <title>Mtaji</title>
-      </Head>
+    return (
+        <>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Poppins"
+                />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                />
+                <title>Mtaji</title>
+            </Head>
 
-      <Component {...pageProps} />
+            <Component {...pageProps} />
 
-      <Footer />
-    </>
-  );
+            <Footer />
+        </>
+    );
 }
 
 export default MyApp;
