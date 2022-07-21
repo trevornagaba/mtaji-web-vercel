7bbfb1a8e00fa130389e0dea415a536b52a33320
Oluwatomisin-code <tomisintomori@gmail.com>
2022-07-21 21:21:50 +0100

    prep for merge

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
             <Navbar />
             <TaglineSection />
             <BenefitsSection />
-            {/* <CompaniesSection /> */}
+            <CompaniesSection />
             <RaiseFunds />
             <Footer/>
         </>
