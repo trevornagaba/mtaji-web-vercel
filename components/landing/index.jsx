1a2f30bdbde93a123ea642017ba604c9c61aca25
Zack Atama <atama.zack@gmail.com>
2022-07-26 17:35:31 +0300

    handle exception

--- components/landing/index.jsx
+++ components/landing/index.jsx
@@ -64,7 +64,7 @@ const Landing = () => {
             })
             .catch((error) => {
                 console.log(error);
-                setCompanies("$");
+                setCompanies([]);
             });
     }
 
