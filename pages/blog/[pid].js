cf33a6450dea66858cc73483db5b4f1ea7171594
Oluwatomisin-code <tomisintomori@gmail.com>
2022-07-27 20:18:14 +0100

    tailwind interpretation of .md not working yet

--- pages/blog/[pid].js
+++ pages/blog/[pid].js
@@ -4,23 +4,24 @@ import { Posts } from "../../components/Blog/post";
 import PageTemplate from "../../components/pageTemplate";
 import ProfileImg from "../../components/ProfileImageIcon";
 import styles from "../../styles/blog.module.css";
+import axios from "axios";
 
 export const getServerSideProps = async (context) => {
     const id = context.query.pid;
-    const post = Posts.filter((post) => {
-        if (post.id.toString() == id.toString()) {
-            return true;
-        }
-        return false;
-    });
-
+    // const post = Posts.filter((post) => {
+    //     if (post.id.toString() == id.toString()) {
+    //         return true;
+    //     }
+    //     return false;
+    // });
+    const post = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/blogs/${id}`)
     return {
-        props: { post },
+        props: { post:post.data },
     };
 };
 const PostDetails = ({ post }) => {
     //convert post into object
-    post = Object.assign({}, ...post);
+    // post = Object.assign({}, ...post);
     console.log(post);
     return (
         <PageTemplate
@@ -58,10 +59,11 @@ const PostDetails = ({ post }) => {
                                 title={post.authorTitle}
                             />
                         </div>
-                        <div className={styles.body}>
+                        <div className="prose">
                             {/* <img src={post.imgUrl} alt="post image"/> */}
                             {/* <div className="product-des" dangerouslySetInnerHTML={{ __html: post.body }}/> */}
-                            {parse(post.body)}
+                            {/* {parse(post.body)} */}
+                            {post.body}
                         </div>
                     </div>
                 </div>
