d48ecf243145655e4dd36d5a62334d8979e54a07
Oluwatomisin-code <tomisintomori@gmail.com>
2022-09-09 00:12:48 +0100

    testing

--- pages/company/[pid].js
+++ pages/company/[pid].js
@@ -68,7 +68,7 @@ export default function Company({ company }) {
         setUser(userDetails)
         
         // console.log(user)
-    },[userDetails])
+    },[isLoaded])
 
     // Setup state management for Investment modal
     const [isOpen, setIsOpen] = useState(false);
