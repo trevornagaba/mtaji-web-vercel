f55f794dd0e8f34026ff2b359e9d28449aa8516b
nagaba <trevornagaba@gmail.com>
2022-09-24 16:38:36 +0100

    Test netlify url when using img tag for happy investor image

--- components/HomeLogo.jsx
+++ components/HomeLogo.jsx
@@ -2,14 +2,14 @@ import styles from "../styles/HomeLogo.module.css";
 import Link from "next/link";
 
 const HomeLogo = () => {
-  return (
-    <Link href="/">
-      <div className={styles.logo}>
-        <img src="/assets/logo.svg" alt="logo" />
-        <span>mtaji</span>
-      </div>
-    </Link>
-  );
+    return (
+        <a href="/">
+            <div className={styles.logo}>
+                <img src="/assets/logo.svg" alt="logo" />
+                <span>mtaji</span>
+            </div>
+        </a>
+    );
 };
 
 export default HomeLogo;
