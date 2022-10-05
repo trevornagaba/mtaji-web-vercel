e8627dc42db68c5c6485d8a3dc71939cb6468a88
Oluwatomisin-code <tomisintomori@gmail.com>
2022-10-05 02:20:28 +0100

    comment landing

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
 
 import AppContextProvider from "../components/AppContext"
 
-import Header from "../components/Header";
-import Landing from "../components/landing";
-import PageTemplate from "../components/pageTemplate";
-import BenefitsSection from "../components/landing/BenefitsSection";
-import CompaniesSection from "../components/landing/CompaniesSection";
-import RaiseFunds from "../components/RaiseFunds";
-import Footer from "../components/Footer";
-import Link from "next/link";
-import Modal from "../components/ModalComponent";
+// import Landing from "../components/landing";
 import Head from "next/head";
 
 export default function Home() {
@@ -20,7 +12,7 @@ export default function Home() {
                 <title>Welcome to Mtaji</title>
                 <meta name="description" content="Equity Crowdfunding for Africans" />
             </Head>
-            <Landing />
+            {/* <Landing /> */}
         </>
         // </PageTemplate>
     );
